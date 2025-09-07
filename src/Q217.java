3c22800715cd557bb1a377d2cc4e8dc9ba62e6fc
Chief A <aathilr7@gmail.com>
2025-09-07 21:40:34 +0530

    Q217/Contains Duplicate/Improved Set approach

--- src/Q217.java
+++ src/Q217.java
@@ -1,19 +1,29 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Q217 {
     class Solution {
 
-        //Sorting approach.
+        //Improved Set approach
         public boolean containsDuplicate(int[] nums) {
-            if(nums.length<2) return false;
-            Arrays.sort(nums);
-            for(int i=0; i<nums.length-1; i++){
-                if(nums[i]==nums[i+1]) return true;
+            Set<Integer> set = new HashSet<>();
+            for(int n: nums){
+                if(!set.add(n)) return true;
             }
             return false;
         }
+
+//        //Sorting approach.
+//        public boolean containsDuplicate(int[] nums) {
+//            if(nums.length<2) return false;
+//            Arrays.sort(nums);
+//            for(int i=0; i<nums.length-1; i++){
+//                if(nums[i]==nums[i+1]) return true;
+//            }
+//            return false;
+//        }
 
 //        //Set approach
 //        public boolean containsDuplicate(int[] nums) {
