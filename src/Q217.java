7ae5fabbb458ca7c215efc547c608606c64d1147
Chief A <aathilr7@gmail.com>
2025-09-07 21:39:17 +0530

    Q217/Contains Duplicate/Sorting approach

--- src/Q217.java
+++ src/Q217.java
@@ -4,9 +4,22 @@ import java.util.stream.Collectors;
 
 public class Q217 {
     class Solution {
+
+        //Sorting approach.
         public boolean containsDuplicate(int[] nums) {
-            Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-            return set.size()!=nums.length;
+            if(nums.length<2) return false;
+            Arrays.sort(nums);
+            for(int i=0; i<nums.length-1; i++){
+                if(nums[i]==nums[i+1]) return true;
+            }
+            return false;
         }
+
+//        //Set approach
+//        public boolean containsDuplicate(int[] nums) {
+//            Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+//            return set.size()!=nums.length;
+//        }
+
     }
 }
