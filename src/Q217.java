cb3d4e7cb43f1d99c43780cf2a0642f0d5f8fbe0
Chief A <aathilr7@gmail.com>
2025-09-07 21:23:12 +0530

    Q217/Contains Duplicate/Set approach

---
+++ src/Q217.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Q217 {
+    class Solution {
+        public boolean containsDuplicate(int[] nums) {
+            Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+            return set.size()!=nums.length;
+        }
+    }
+}
