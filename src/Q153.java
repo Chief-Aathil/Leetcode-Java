e384aba0f31edde0d200dad7a0e2c83120f94e09
Chief A <aathilr7@gmail.com>
2025-09-11 09:00:58 +0530

    Q153/Find Minimum in Rotated Sorted Array/Binary search

--- src/Q153.java
+++ src/Q153.java
@@ -5,11 +5,25 @@ import java.util.stream.Collectors;
 public class Q153 {
     class Solution {
         public int findMin(int[] nums) {
-            return Arrays.stream(nums)
-                    .boxed()
-                    .collect(Collectors.toCollection(PriorityQueue::new))
-                    .poll();
+            //binary search: compare middle with left and right
+            int left = 0, right=nums.length-1,mid,smallest=nums[0];
+            int currentSmallest=smallest;
+            while(left<right){
+                mid= (left+right)/2;
+                int prev = nums[left];
+                int next = nums[right];
+                currentSmallest=Math.min(Math.min(prev,next),nums[mid]);
+                if(smallest<currentSmallest)break;
+                smallest = currentSmallest;
 
+                if(smallest==prev || smallest == nums[mid]){
+                    right=mid-1;
+                }else if(smallest==next){
+                    left=mid+1;
+                }
+            }
+            return smallest;
         }
     }
+
 }
