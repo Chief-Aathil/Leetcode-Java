78f6b8a3bb1344c69d6738937d2bdc73e9fbebdd
Chief A <aathilr7@gmail.com>
2025-09-15 08:17:44 +0530

    Q347/Top K Frequent Elements/Frequency Buckets approach

--- src/Q347.java
+++ src/Q347.java
@@ -1,33 +1,41 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Q347 {
     class Solution {
         public int[] topKFrequent(int[] nums, int k) {
-            /** Populate a maxheap. Elements: [num,freq]. Set freq value as the sorting condition
+            /**
+             Populate an inverted frequence map.
+             Give the top k elements from this
              */
 
-            //Find frequency of numbers
-            Map<Integer,Integer> freqMap = new HashMap<>();
-            for(int n : nums){
+            //Create a map of n:freq
+            Map<Integer, Integer> freqMap = new HashMap<>();
+            for(int n: nums){
                 freqMap.put(n,freqMap.getOrDefault(n,0)+1);
             }
 
-            //Create max-heap from freqMap
-            PriorityQueue<Map.Entry<Integer,Integer>> heap = new PriorityQueue<>((a, b)->
-                    b.getValue()-a.getValue()
-            );
+            //Create a frequency bucket[freq:[nums that have freq]]
+            List<Set<Integer>>freqBuckets = new ArrayList<>(nums.length);//optimize size
+            for(int i=0; i<nums.length; i++){
+                freqBuckets.add(new HashSet<>());
+            }
             for(var entry: freqMap.entrySet()){
-                heap.add(entry);
+                var set = freqBuckets.get(entry.getValue()-1);
+                set.add(entry.getKey());
+                freqBuckets.set(entry.getValue()-1,set);
             }
 
-            //Take k elements from the max heap and put only the keys in the response
-            int[] output = new int[k];
-            for(int i=0; i<k; i++){
-                output[i]= heap.poll().getKey();
+            //Take last k elements from freqBucket
+            int[] out = new int[k];
+            for(int i=0,j=freqBuckets.size()-1;i<k;j--){
+                var set = freqBuckets.get(j);
+                if(set.isEmpty()) continue;
+                for(var e: set){
+                    out[i++] = e;
+                }
             }
-            return output;
+            return out;
+
         }
     }
 }
