1164cec79c8f8ceb77a70eba4de7313916212d62
Chief A <aathilr7@gmail.com>
2025-09-22 06:52:42 +0530

    Q424/Longest Repeating Character Replacement/Dynamic sliding window approach-working

--- src/Q424.java
+++ src/Q424.java
@@ -5,27 +5,28 @@ public class Q424 {
     class Solution {
         public int characterReplacement(String s, int k) {
             /**
-             Dynamic sliding window with atmost k+1 distinct elements.
-             Max window size is the output.
+             Dynamic sliding window. Max window size where count of non-dominant characters<=k
              */
-
-            char[] chars = s.toCharArray();
-            int left=0,right=1,max=1;
+            int left=0, right=0,max=0;
             Map<Character,Integer> charMap = new HashMap<>();
-            charMap.put(chars[left],1);
-            while(right<chars.length){
-                if(chars[left]!=chars[right]){
-                    charMap.put(chars[right],charMap.getOrDefault(chars[right],0)+1);
-                }
+            char[] chars = s.toCharArray();
+            while(right<s.length()){
+                charMap.put(chars[right],charMap.getOrDefault(chars[right],0)+1);
                 right++;
 
-                if(charMap.size()>k+1){
-                    if(charMap.get(chars[left])==1){
-                        charMap.remove(chars[left]);
-                    }
-                    left++;
+                //Find dominant-character:
+                int dominantCharCount =0;
+                for(int i=left; i<right; i++){
+                    dominantCharCount = Math.max(dominantCharCount,charMap.get(chars[i]));
                 }
-                max=Math.max(max,right-left);
+
+                //Check if non-dominant character count<=k
+                if(right-left -dominantCharCount >k){
+                    charMap.put(chars[left], charMap.get(chars[left])-1);
+                    left++;
+                }else{
+                    max=Math.max(max,right-left);
+                }
             }
             return max;
         }
