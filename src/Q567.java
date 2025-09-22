3085efb706dc095f83ecbda50e9235bb273ed273
Chief A <aathilr7@gmail.com>
2025-09-22 11:37:51 +0530

    Q567/Permutation in String/Sliding window-working

--- src/Q567.java
+++ src/Q567.java
@@ -2,54 +2,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Q567 {
-    class Solution {
-        public boolean checkInclusion(String s1, String s2) {
-            /**
-             Create character frequency map of s1.
-             Iterate s2 till a matching character is found.
-             Expand window right till a non-matching character
-             (either different character or freq mismatch)is found or entire s1 is matched.
-             Do till end of s2 is in window
-             */
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        var out = obj.checkInclusion("adc","dcda");
+        System.out.println(out);
+    }
+}
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
+        Map<Character, Integer> s1Map = new HashMap<>();
+        Map<Character, Integer> windowMap = new HashMap<>();
 
-            //Create char-freq map of s1
-            char[] chars1 = s1.toCharArray();
-            Map<Character,Integer> charMap = new HashMap<>();
-            for(char c: chars1){
-                charMap.put(c,charMap.getOrDefault(c,0)+1);
-            }
-
-            //Iterate through s2 till last char in window
-            int left=0, right=0;
-            while(right<s2.length()){
-                //increment left till matching char
-                while(left<s2.length() && charMap.get(s2.charAt(left))==null){
-                    left++;
-                }
-                if(left>=s2.length()){
-                    return false;
-                }
-
-                //Matching char found. Increment right till complete match or unmatch.
-                right = left;
-                Map<Character, Integer> subStringMap = new HashMap<>();
-                while(right<s2.length()){
-                    char c = s2.charAt(right);
-                    subStringMap.put(c,subStringMap.getOrDefault(c,0)+1);
-                    //check for character or freq mismatch
-                    if(charMap.get(c)!=null || charMap.get(c)>=subStringMap.get(c)){
-                        break;
-                    }
-                    right++;
-                }
-                if(s1.length()==right-left+1){
-                    return true;
-                }else{
-                    left = right;
-                }
-
-            }
-            return false;
+        //Build s1char map and windowMap
+        for (int i = 0; i < s1.length(); i++) {
+            s1Map.put(s1.charAt(i),s1Map.getOrDefault(s1.charAt(i),0)+1);
+            windowMap.put(s2.charAt(i),windowMap.getOrDefault(s2.charAt(i),0)+1);
         }
+
+        //Slide window
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if(s1Map.equals(windowMap))
+                return true;
+            var leftChar = s2.charAt(i);
+            var rightChar = s2.charAt(s1.length()+i);
+
+            windowMap.put(rightChar,windowMap.getOrDefault(rightChar,0)+1);
+            windowMap.put(leftChar,windowMap.get(leftChar)-1);
+            if(windowMap.get(leftChar)==0){
+                windowMap.remove(leftChar);
+            }
+        }
+        return s1Map.equals(windowMap);
     }
 }
