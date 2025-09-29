d24dfa7d50575ffdacdf44fa0096d8275a2415e4
Chief A <aathilr7@gmail.com>
2025-09-29 10:46:35 +0530

    Q572/Subtree of Another Tree/Serialization approach

--- src/Q572.java
+++ src/Q572.java
@@ -21,17 +21,14 @@ public class Q572 {
 
     class Solution {
         public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-            if (root == null && subRoot == null) return true;
-            if (root != null && subRoot == null || root == null && subRoot != null) return false;
-            var isCurrentRootsMatch = dfs(root, subRoot);
-            return isCurrentRootsMatch || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+            String subTreeString = serialize(subRoot);
+            String mainTreeString = serialize(root);
+            return mainTreeString.contains(subTreeString);
         }
 
-        boolean dfs(TreeNode root, TreeNode subRoot) {
-            if (root == null && subRoot == null) return true;
-            if (root != null && subRoot == null || root == null && subRoot != null) return false;
-            if (root.val != subRoot.val) return false;
-            return dfs(root.left, subRoot.left) && dfs(root.right, subRoot.right);
+        String serialize(TreeNode node){
+            if(node == null) return "#N";
+            return "#" + node.val + ","+ serialize(node.left)+","+serialize(node.right);
         }
     }
 }
