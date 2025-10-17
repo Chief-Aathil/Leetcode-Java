4e56bcba437f0560e3138d6b42cd3d883433f6d0
Chief A <aathilr7@gmail.com>
2025-10-17 08:54:51 +0530

    Q56/Merge Intervals/Initial approach-Simplified

--- src/Q56.java
+++ src/Q56.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,29 +9,19 @@ public class Q56 {
             //Sort the intervals based on start
             Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
 
-            List<List<Integer>> out = new LinkedList<>();
+            List<int[]> out = new ArrayList<>();
             for (int i = 0, n = intervals.length; i < n; i++) {
                 int start = intervals[i][0];
                 int end = intervals[i][1];
 
                 //Merge adjacent intervals with overlap
                 while (i + 1 < n && end>=intervals[i+1][0]) {
-                    if(end<intervals[i+1][1]){
-                        end = intervals[i+1][1];
-                    }
+                    end=Math.max(end, intervals[i+1][1]);
                     i++;
                 }
-                out.add(List.of(start, end));
+                out.add(new int[]{start, end});
             }
-
-            //create output array.
-            int[][] outArr = new int[out.size()][2];
-            for (int i = 0; i < out.size(); i++) {
-                outArr[i][0] = out.get(i).get(0);
-                outArr[i][1] = out.get(i).get(1);
-            }
-            return outArr;
+            return out.toArray(new int[out.size()][2]);
         }
     }
-
 }
