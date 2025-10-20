2f143de2444aa017eb385738a3fc2f25d661b8b7
Chief A <aathilr7@gmail.com>
2025-10-20 08:22:28 +0530

    Q238/Product of Array Except Self/Prefix and Suffix product-optimized space.

--- src/Q238.java
+++ src/Q238.java
@@ -2,18 +2,14 @@ public class Q238 {
     class Solution {
         public int[] productExceptSelf(int[] nums) {
             int[] out = new int[nums.length];
-            int[] suffix = new int[nums.length];
             int prefixProduct=1, suffixProduct=1;
             for(int i=0; i<nums.length; i++){
                 out[i] = prefixProduct;
                 prefixProduct*=nums[i];
             }
             for(int i=nums.length-1; i>=0; i--){
-                suffix[i]= suffixProduct;
+                out[i]*=suffixProduct;
                 suffixProduct*=nums[i];
-            }
-            for(int i=0; i<nums.length; i++){
-                out[i] = out[i]*suffix[i];
             }
             return out;
         }
