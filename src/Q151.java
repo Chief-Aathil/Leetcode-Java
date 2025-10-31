88a33832e0d1828d1be664b0b3b32a40603e0eb4
Chief A <aathilr7@gmail.com>
2025-10-31 07:50:32 +0530

    Q151/Reverse Words in a String/Corrected approach

--- src/Q151.java
+++ src/Q151.java
@@ -1,19 +1,16 @@
+import java.util.Arrays;
+
 public class Q151 {
     class Solution {
         public String reverseWords(String s) {
             //Split the string on ' '
-            char[] chars = s.toCharArray();
+            String[] words = s.trim().split(" +");
             StringBuilder builder = new StringBuilder();
-            for(int i=0; i<chars.length; i++){
-                //find start and end indices of word
-                while(chars[i++]!=' ' && i<chars.length);
-                i--;
-                int j=i<chars.length-1? i+1: i;
-                for(; j<chars.length && chars[j]!=' '; j++);
 
-                //insert the word to the beginning
-                builder.insert(0,new String(Arrays.copyOfRange(chars,i,j+1)));
-                i=j;
+            //Add words in reverse order
+            for(int i=words.length-1; i>=0; i--){
+                builder.append(words[i]);
+                if(i>0) builder.append(" ");
             }
             return builder.toString();
         }
