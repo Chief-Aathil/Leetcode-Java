afccdc626e0cbbecd4fe17875580b975305d2905
Chief A <aathilr7@gmail.com>
2025-10-31 07:05:44 +0530

    Q79/Word Search/Corrected approach

--- src/Q79.java
+++ src/Q79.java
@@ -1,53 +1,44 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Q79 {
+    public  record Pair(Integer i, Integer j){};
     class Solution {
         public boolean exist(char[][] board, String word) {
-            //iterate through the board till first character is found
+            Set<Pair> visited = new HashSet<>();
+            //Iterate through the board for first character
             for(int i=0; i<board.length; i++){
-                for(int j=0; j<board[i].length; j++){
-                    //recursively call check() of valid neighbours
-                    if(board[i][j]==word.charAt(0) && check(board, i, j, word, 0,new ArrayList<>())){
-                        return true;
-                    }
+                for(int j=0; j<board[0].length; j++){
+                    if(check(board, word, i, j, visited)) return true;
                 }
             }
             return false;
         }
 
-        boolean check(char[][] board, int i, int j, String word,int charIndex, List<Integer> visitedIndices){
-            //check for word fully present
-            if(word.length()-1 == charIndex) return true;
-
-            //find valid neighbours
-            int index = i*board.length + j;
-            int[] indices = new int[]{ index-1, index+1, index-board.length, index+board.length};
-            for(int idx: indices){
-                int rowIndex= idx/board.length;
-                int colIndex= idx % board.length;
-                if(isValidIndex(board, rowIndex, colIndex, word, charIndex, visitedIndices)){
-                    visitedIndices.add(idx);
-                    if(check(board, rowIndex, colIndex, word, charIndex+1, visitedIndices)){
-                        return true;
-                    }
-                    visitedIndices.remove(visitedIndices.size()-1);
-                }
+        //Recursive function
+        boolean check(char[][] board, String word, int i, int j, Set<Pair> visited) {
+            //check if valid index, visited indices, current character match
+            if (i < 0 || i >= board.length || j < 0 || j >= board[0].length
+                    || visited.contains(new Pair(i, j)) || board[i][j] != word.charAt(visited.size())) {
+                return false;
             }
-            return false;
-        }
 
-        boolean isVisited(int i, int j, List<Integer> visitedIndices, int rowLength){
-            for(var index: visitedIndices){
-                if(index/rowLength == i && index%rowLength == j) return true;
-            }
-            return false;
-        }
+            //check if entire word matched
+            if (word.length() == visited.size() + 1)
+                return true;
 
-        boolean isValidIndex(char[][] board, int i, int j, String word,int charIndex, List<Integer> visitedIndices){
-            return i>=0 && i<board.length && j>=0 && j<board.length
-                    && board[i][j] == word.charAt(charIndex)
-                    && !isVisited(i,j,visitedIndices,board.length);
+            //check neighbours
+            visited.add(new Pair(i, j));
+            if (check(board, word, i - 1, j, visited) ||
+                    check(board, word, i + 1, j, visited) ||
+                    check(board, word, i, j - 1, visited) ||
+                    check(board, word, i, j + 1, visited))
+                return true;
+            visited.remove(new Pair(i, j));
+            return false;
+
         }
     }
 }
