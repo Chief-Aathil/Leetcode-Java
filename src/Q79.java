d48e5467d8420e0d2ad09ac8205d03d8444b82c0
Chief A <aathilr7@gmail.com>
2025-11-03 08:37:09 +0530

    Q79/Word Search/Using boolean 2d array for marking visited cells.

--- src/Q79.java
+++ src/Q79.java
@@ -4,39 +4,39 @@ import java.util.List;
 import java.util.Set;
 
 public class Q79 {
-    public  record Pair(Integer i, Integer j){};
     class Solution {
         public boolean exist(char[][] board, String word) {
-            Set<Pair> visited = new HashSet<>();
+            boolean[][] visited = new boolean[board.length][board[0].length];
             //Iterate through the board for first character
-            for(int i=0; i<board.length; i++){
-                for(int j=0; j<board[0].length; j++){
-                    if(check(board, word, i, j, visited)) return true;
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (check(board, word, i, j, visited, 0))
+                        return true;
                 }
             }
             return false;
         }
 
         //Recursive function
-        boolean check(char[][] board, String word, int i, int j, Set<Pair> visited) {
+        boolean check(char[][] board, String word, int i, int j, boolean[][] visited, int charIndex) {
             //check if valid index, visited indices, current character match
             if (i < 0 || i >= board.length || j < 0 || j >= board[0].length
-                    || visited.contains(new Pair(i, j)) || board[i][j] != word.charAt(visited.size())) {
+                    || visited[i][j] || board[i][j] != word.charAt(charIndex)) {
                 return false;
             }
 
             //check if entire word matched
-            if (word.length() == visited.size() + 1)
+            if (charIndex == word.length())
                 return true;
 
             //check neighbours
-            visited.add(new Pair(i, j));
-            if (check(board, word, i - 1, j, visited) ||
-                    check(board, word, i + 1, j, visited) ||
-                    check(board, word, i, j - 1, visited) ||
-                    check(board, word, i, j + 1, visited))
+            visited[i][j] = true;
+            if (check(board, word, i - 1, j, visited, charIndex + 1) ||
+                    check(board, word, i + 1, j, visited, charIndex + 1) ||
+                    check(board, word, i, j - 1, visited, charIndex + 1) ||
+                    check(board, word, i, j + 1, visited, charIndex + 1))
                 return true;
-            visited.remove(new Pair(i, j));
+            visited[i][j] = false;
             return false;
 
         }
