6c3dd8260e7cf71867cc094a08b0d2504657b054
Chief A <aathilr7@gmail.com>
2025-11-07 08:14:08 +0530

    Q80/Remove Duplicates from Sorted Array II/2 pointer approach - simplified and optimal

--- src/Q80.java
+++ src/Q80.java
@@ -1,27 +1,13 @@
 public class Q80 {
-//    class Solution {
-//        public int removeDuplicates(int[] nums) {
-//            int left=0, right=0,k=0, currentNum, currentCount=0;
-//            while(left<nums.length-k){
-//                currentNum = nums[right];
-//
-//                //find last valid index(left) of current num and index(right) of next num
-//                while(nums[right] == currentNum){
-//                    if(currentCount<3){
-//                        left++;
-//                        k++;
-//                    }
-//                    currentCount++;
-//                    right++;
-//                }
-//
-//                //replace duplicate indices after left with number at right
-//                if(currentCount>2){
-//                    while(nums)
-//                        nums[++left] = nums[right++]
-//                }
-//
-//            }
-//        }
-//    }
+    class Solution {
+        public int removeDuplicates(int[] nums) {
+            int k=0;
+            for(int i=0; i<nums.length; i++){
+                if(i<2 || nums[i]!=nums[k-2]){
+                    nums[k++] = nums[i];
+                }
+            }
+            return k;
+        }
+    }
 }
