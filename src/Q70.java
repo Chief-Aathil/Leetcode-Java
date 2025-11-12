8e199e6f7f2c67c3e7b5d60d7ad768ab5e0494be
Chief A <aathilr7@gmail.com>
2025-11-12 08:01:45 +0530

    Q70/Climbing Stairs/Recursive DP- simplified

--- src/Q70.java
+++ src/Q70.java
@@ -2,26 +2,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Q70 {
-    class Solution {
+    static class Solution {
         Map<Integer,Integer> map = new HashMap<>();
 
         public int climbStairs(int n) {
             if(n<3) return n;
             if(map.containsKey(n)) return map.get(n);
-            int a,b;
-            if(map.containsKey(n-2)){
-                b= map.get(n-2);
-            }else{
-                b= climbStairs(n-2);
-                map.put(n-2,b);
-            }
-            if(map.containsKey(n-1)){
-                a=map.get(n-1);
-            }else{
-                a=climbStairs(n-1);
-                map.put(n-1,a);
-            }
+            int a= climbStairs(n-2);
+            int b = climbStairs(n-1);
+            map.put(n,a+b);
             return a+b;
         }
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        var out = obj.climbStairs(44);
+        System.out.println(out);
+
     }
 }
