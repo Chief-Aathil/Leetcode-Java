ef727ed2eb8813a7d3894bef7c97c5194c791328
Chief A <aathilr7@gmail.com>
2025-11-19 07:30:30 +0530

    Q2154/Keep Multiplying Found Values by Two/Set-approach

---
+++ src/Q2154.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Q2154 {
+    class Solution {
+        public int findFinalValue(int[] nums, int original) {
+            Set<Integer> set = new HashSet<>();
+            for(int n: nums) set.add(n);
+            while(set.contains(original)){
+                original*=2;
+            }
+            return original;
+        }
+    }
+}
