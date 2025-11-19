3fab8623d8a9b301955a18e1d7e31ffec519f5b6
Chief A <aathilr7@gmail.com>
2025-11-19 11:55:21 +0530

    Q15/3Sum/Sort+2Pointer approach-corrected

--- src/Q15.java
+++ src/Q15.java
@@ -6,18 +6,22 @@ public class Q15 {
             Arrays.sort(nums);
             List<List<Integer>> output = new ArrayList<>();
             for(int i=0; i<nums.length-2; i++){
-                if(nums[i]==0) break;
+                if(nums[i]>0) break;
                 if(i>0 && nums[i]== nums[i-1]) continue;
 
                 // For each num find2Sum
                 int left=i+1, right= nums.length-1;
+                int target = -nums[i], currentSum;
                 while(left<right){
-                    if(nums[left]+nums[right]> -nums[i]) right--;
-                    else if(nums[left]+nums[right]< -nums[i]) left++;
+                    currentSum = nums[left] + nums[right];
+                    if(currentSum > target) right--;
+                    else if(currentSum < target) left++;
                     else {
                         output.add(List.of(nums[i],nums[left],nums[right]));
-                        while(left<right && nums[left]==nums[left+1]) left++;
-                        while(left<right && nums[right] == nums[right-1]) right--;
+                        left++;
+                        right--;
+                        while(left<right && nums[left]==nums[left-1]) left++;
+                        while(left<right && nums[right] == nums[right+1]) right--;
                     }
                 }
             }
