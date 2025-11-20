0742292a47b57bdb31311155cba77ae9818fea5f
Chief A <aathilr7@gmail.com>
2025-11-20 13:07:30 +0530

    Q121/Best Time to Buy and Sell Stock/Greedy approach

--- src/Q121.java
+++ src/Q121.java
@@ -1,26 +1,14 @@
 public class Q121 {
     class Solution {
         public int maxProfit(int[] prices) {
-            //Sliding window:
-            //buyDay:lowest price so far
-            //sellDay: highest price after buyDay
-
-            //dayPrice>sellDayPrice>buyDayPrice: update sellDay
-            //dayPrice<buyDayPrice: update buyDay and sellDay
-
-            int buyDay=0, sellDay=1, maxProfit=0;
-            for(int i=1; i<prices.length; i++){
-                if(prices[i]>prices[buyDay]){
-                    sellDay = prices[sellDay] > prices[i] ? sellDay: i;
-                }else if(prices[i]<prices[buyDay]){
-                    buyDay = i;
-                    sellDay = i+1;
+            int buyPrice = prices[0];
+            int maxProfit = 0;
+            for(int price: prices){
+                if(price>buyPrice){
+                    maxProfit= Math.max(maxProfit, price-buyPrice);
+                }else if(price<buyPrice){
+                    buyPrice = price;
                 }
-
-                if(buyDay<prices.length && sellDay<prices.length && buyDay<sellDay){
-                    maxProfit = Math.max(maxProfit,prices[sellDay]-prices[buyDay]);
-                }
-
             }
             return maxProfit;
         }
